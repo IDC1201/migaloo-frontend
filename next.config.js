488c2fca13ebb04f55bc84f1b69138e6a84d0d4e
IDC <113762983+IDC1201@users.noreply.github.com>
2022-12-17 14:35:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,52 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const withBundleJunoblocks = require('next-bundle-junoblocks')
 
-/**
- * @0xFable - Suppress errant recoil errors which seem to occur due to hot reloading
- * When using the app there is constant recoil warnings in the logs that users could see which indicate
- * almost fatal issues with the state.
- * Rather than this meaning an issue, on multiple SSR based frameworks there is an issue noted with seeing this on refreshes
- * possible because the state/pages are being loaded with hot module replacement
- * For more info go here: https://github.com/facebookexperimental/Recoil/issues/733
- */
-const intercept = require('intercept-stdout')
-
-// safely ignore recoil stdout warning messages
-function interceptStdout(text) {
-  if (text.includes('Duplicate atom key')) {
-    return ''
-  }
-  return text
-}
-
-// Intercept in dev and prod
-intercept(interceptStdout)
-
 const config = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/swap',
-        permanent: true,
-      },
-    ]
-  },
-  reactStrictMode: false,
+  reactStrictMode: true,
   target: 'serverless',
-  // TODO: Needed to disabled all build error checking and eslint checking due to error with usage of Controller component, to fix that needs to be fixed
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dk8s7xjsl/image/upload/',
